3894835eb3bc94a1e70e62e1151b949be835fba2
msq <57103654+jerryjames641@users.noreply.github.com>
2024-12-19 22:41:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,16 +5,8 @@ const nextButton = document.getElementById('next');
 const trackTitle = document.getElementById('track-title');
 
 let trackList = [
-    { title: 'Nessaja', src: 'song 1.mp3' },
-    { title: 'Amazing ', src: 'song 2.mp3' },
-    { title: 'Weekend', src: 'song 3.mp3' },
-    { title: 'What is love', src: 'song 4.mp3' },
-    { title: 'Around the world', src: 'song 5.mp3' },
-    { title: 'Get a way', src: 'song 6.mp3' },
-    { title: 'Beautiful', src: 'song 7.mp3' },
-    { title: 'Mr Saxobeat', src: 'song 8.mp3' },
-    { title: 'How much is the fish', src: 'song 9.mp3' },
-    { title: 'Back in my life', src: 'song 10.mp3' },
+    { title: 'Zaara', src: 'song11.mp3' },
+   
 
 
 ];
